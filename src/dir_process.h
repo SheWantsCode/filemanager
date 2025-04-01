608f0b372287e7ff420ff06458a4b54b57b8495b
SheWantsCode <sasha_orlov_132@mail.ru>
2025-04-01 13:58:46 +0300

    Add prototypes for future features

--- src/dir_process.h
+++ src/dir_process.h
@@ -3,8 +3,15 @@
 
 #include <stdbool.h>
 #include <sys/stat.h>
+#include "cli_parser.h"
 
 char* show_dir(char* path, char* file_name, bool lname_flag);
+
+// In make_path func make path for file/directory with options in CliArgs
+void make_path(cliArgs cliArgs, char* full_path);
+
+// In the cut_path function cut out the path to the file, leaving only name of file
+char* cut_path(char* full_path);
 
 void print_perms(mode_t perms);
 
